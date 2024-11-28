03b9afd25ef9613cefa7594b74eddbc2be7b8663
Conor <conor@myrtle.com>
2024-11-28 16:01:25 +0000

    no column prune

--- data.py
+++ data.py
@@ -2,7 +2,6 @@ import kagglehub
 import os
 import pandas as pd
 
-from hashlib import md5
 from sklearn.preprocessing import LabelEncoder
 
 
@@ -12,23 +11,19 @@ path = kagglehub.dataset_download(
     "mohamedamineferrag/edgeiiotset-cyber-security-dataset-of-iot-iiot"
 )
 
-
 print("Loading dataset...")
 
 path = os.path.join(
     path, "Edge-IIoTset dataset/Selected dataset for ML and DL/DNN-EdgeIIoT-dataset.csv"
 )
 
-
 df = pd.read_csv(path, low_memory=False)
 
-# Print column names
 print("Columns:")
 for col in df.columns:
     print("\t", col)
 
 print("Encoding categorical features...")
-
 
 keys = [
     "http.request.method",
@@ -45,7 +40,6 @@ enc = pd.DataFrame()
 for key in keys:
     enc[key] = LabelEncoder().fit_transform(df[key])
 
-
 print("One-hot encoding...")
 
 one_hot = pd.concat(
@@ -57,45 +51,10 @@ df = pd.concat([df, one_hot], axis=1)
 
 print("Cleaning data...")
 
-df = df.drop_duplicates()
-
-
-# Function to create a hash for each column
-def hash_column(series):
-    return md5(pd.util.hash_pandas_object(series, index=False).values).hexdigest()
-
-
-# Function to find columns with identical hashes
-def find_identical_columns_by_hash(df):
-
-    hash_dict = {}
-
-    for col in df.columns:
-
-        col_hash = hash_column(df[col])
-
-        if col_hash in hash_dict:
-            hash_dict[col_hash].append(col)
-        else:
-            hash_dict[col_hash] = [col]
-
-    return [cols for cols in hash_dict.values() if len(cols) > 1]
-
-
-# Applying the function to the DataFrame
-identical_column_groups = find_identical_columns_by_hash(df)
-
-# Sort by time for dev/test splits later
+# This sort is needed for later time-based splits
 df = df.sort_values("frame.time")
 
-
 print("Dropping columns...")
-
-print("Groups of identical columns:")
-for group in identical_column_groups:
-    print("\t", group)
-    df = df.drop(group[1:], axis=1)
-
 
 drop_columns = [
     "frame.time",
@@ -114,7 +73,6 @@ drop_columns = [
     "udp.port",
     "mqtt.msg",
 ]
-
 
 df.drop(drop_columns, axis=1, inplace=True)
 df.dropna(axis=0, how="any", inplace=True)
@@ -138,7 +96,6 @@ print("\nDev")
 print(dev["Attack_type"].value_counts())
 print("\nTest")
 print(test["Attack_type"].value_counts())
-
 
 print("Saving splits...")
 
